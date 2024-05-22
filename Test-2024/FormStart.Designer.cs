78c88feed59a278c94f8d3296dbea3b8ea328455
teacher <chportko@ukr.net>
2024-05-22 22:21:40 +0300

    Добавьте файлы проекта.

---
+++ Test-2024/FormStart.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Test_2024
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.пІ22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кН22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.іН22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.єВІ22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.пІ22ToolStripMenuItem,
+            this.кН22ToolStripMenuItem,
+            this.іН22ToolStripMenuItem,
+            this.єВІ22ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // пІ22ToolStripMenuItem
+            // 
+            this.пІ22ToolStripMenuItem.Name = "пІ22ToolStripMenuItem";
+            this.пІ22ToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.пІ22ToolStripMenuItem.Text = "ПІ-22";
+            // 
+            // кН22ToolStripMenuItem
+            // 
+            this.кН22ToolStripMenuItem.Name = "кН22ToolStripMenuItem";
+            this.кН22ToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.кН22ToolStripMenuItem.Text = "КН-22";
+            // 
+            // іН22ToolStripMenuItem
+            // 
+            this.іН22ToolStripMenuItem.Name = "іН22ToolStripMenuItem";
+            this.іН22ToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.іН22ToolStripMenuItem.Text = "ІН-22";
+            // 
+            // єВІ22ToolStripMenuItem
+            // 
+            this.єВІ22ToolStripMenuItem.Name = "єВІ22ToolStripMenuItem";
+            this.єВІ22ToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.єВІ22ToolStripMenuItem.Text = "ЄВІ-22";
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тести-2024";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem пІ22ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem кН22ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem іН22ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem єВІ22ToolStripMenuItem;
+    }
+}
+
